dd4510ab2a295acc3e9bcf180b5d3c66ad8133e6
Jorge Alexandro Zamudio Arredondo <alexzamudio_11@hotmail.com>
2023-12-11 08:34:35 -0700

    Add initial state for spaceship pages

---
+++ lib/pages/spaceship_details/view/spaceship_details_page.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+function SpaceshipDetailsPage() {
+  return (
+    <View
+      
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}
+       >
+
+      <Text>Spaceships page</Text>
+    </View>
+  )
+}
+
+export default SpaceshipDetailsPage;
