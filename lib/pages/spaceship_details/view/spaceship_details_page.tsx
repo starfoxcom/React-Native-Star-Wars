655b4326429acc86259c288a3b6e093d946da13d
Jorge Alexandro Zamudio Arredondo <alexzamudio_11@hotmail.com>
2023-12-11 16:10:09 -0700

    Finish pages implementation and fetch data for spaceships pages

--- lib/pages/spaceship_details/view/spaceship_details_page.tsx
+++ lib/pages/spaceship_details/view/spaceship_details_page.tsx
@@ -1,15 +1,74 @@
-import { Text, View } from "react-native";
+import { Image, ImageBackground, ScrollView, StyleSheet, Text, View } from "react-native";
+import { SpaceshipStackScreenProps } from "../../../router/router";
 
-function SpaceshipDetailsPage() {
+
+function SpaceshipDetailsPage({ route }: SpaceshipStackScreenProps<'SpaceshipDetailsPage'>) {
+  const { spaceship, uri } = route.params;
   return (
-    <View
-      
-      style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}
-       >
+    <ScrollView>
+      <View style={{ justifyContent: 'center', alignItems: 'center' }} >
 
-      <Text>Spaceships page</Text>
-    </View>
+        <Text style={styles.title}>{spaceship.name}</Text>
+        <View style={styles.shadow}>
+          <ImageBackground source={
+            { uri: `https://starwars-visualguide.com/assets/img/big-placeholder.jpg` }} imageStyle={styles.imageBackground} >
+            <Image source={{ uri: uri }} style={styles.image} />
+          </ImageBackground>
+        </View>
+        <Text style={styles.text}>Model: {spaceship.model}</Text>
+        <Text style={styles.text}>Manufacturer: {spaceship.manufacturer}</Text>
+        <Text style={styles.text}>Class: {spaceship.starship_class}</Text>
+        <Text style={styles.text}>Cost: {spaceship.cost_in_credits} credits</Text>
+        <Text style={styles.text}>Speed: {spaceship.max_atmosphering_speed}km/h</Text>
+        <Text style={styles.text}>Hyperdrive Rating: {spaceship.hyperdrive_rating}</Text>
+        <Text style={styles.text}>MGLT: {spaceship.MGLT}</Text>
+        <Text style={styles.text}>Length: {spaceship.length}</Text>
+        <Text style={styles.text}>Cargo Capacity: {spaceship.cargo_capacity}</Text>
+        <Text style={styles.text}>Minimum Crew: {spaceship.crew}</Text>
+        <Text style={styles.text}>Passengers: {spaceship.passengers}</Text>
+
+
+      </View>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    padding: 20,
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+
+  text: {
+
+    fontSize: 20,
+    color: 'black',
+  },
+  image: {
+    width: 200,
+    height: 300,
+    marginBottom: 10,
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+
+  imageBackground: {
+    width: 200,
+    height: 300,
+    borderRadius: 100,
+  },
+  shadow: {
+    width: 200,
+    height: 300,
+    marginBottom: 15,
+    borderRadius: 100,
+    shadowColor: 'black',
+    elevation: 5,
+  }
+
+});
 
 export default SpaceshipDetailsPage;
--- lib/pages/spaceships/view/spaceships_page.tsx
+++ lib/pages/spaceships/view/spaceships_page.tsx
@@ -1,13 +1,56 @@
-import { Text, View } from "react-native";
+import { Text, Image, View, TouchableOpacity, ImageBackground } from "react-native";
+import { SpaceshipStackScreenProps } from "../../../router/router";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchSpaceships } from "../../../API/starwars_API";
+import { Spaceships, SpaceshipsResult } from "../../../models/models";
+import { GridView, LoadingSpinner, getUrlId } from "../../../utils/utils";
 
-function SpaceshipsPage() {
+
+function SpaceshipsPage({ navigation }: SpaceshipStackScreenProps<'SpaceshipsPage'>) {
+
+  const [page, setPage] = useState(1);
+
+  const { isPending, isError, data, error } = useQuery<Spaceships>({
+    queryKey: ['spaceships', page],
+    queryFn: () => fetchSpaceships(page),
+  });
+
+  const spaceships: SpaceshipsResult[] = data?.results || [];
+
+  if (isPending) return
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
+    <LoadingSpinner />
+  </View>;
+
+  if (isError) return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
+    <Text>Error: {error.message}</Text>
+  </View>;
+
   return (
-    <View
-      
-      style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}
-       >
+    <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap' }}>
+      <GridView data={spaceships} renderItem={(item) => (
+        <TouchableOpacity
+          style={{ justifyContent: 'center', alignItems: 'center' }}
+          onPress={() => navigation.navigate('SpaceshipDetailsPage', {
+            spaceship: item, uri: `https://starwars-visualguide.com/assets/img/starships/${getUrlId(item.url)}.jpg`
+          })}
+        >
+          <ImageBackground source={
+            { uri: `https://starwars-visualguide.com/assets/img/big-placeholder.jpg` }}
+            imageStyle={{
+              width: 114, height: 110, borderTopLeftRadius: 10, borderTopRightRadius: 10,
+            }} >
 
-      <Text>Spaceships page</Text>
+            <Image source={[{
+              uri: `https://starwars-visualguide.com/assets/img/starships/${getUrlId(item.url)}.jpg`
+            }]} style={{
+              width: 114, height: 110, borderTopLeftRadius: 10, borderTopRightRadius: 10,
+            }} />
+          </ImageBackground>
+          <Text>{item.name}</Text>
+        </TouchableOpacity>
+      )} />
     </View>
   )
 }
