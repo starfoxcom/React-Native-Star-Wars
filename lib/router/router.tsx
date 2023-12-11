018f1ec4e60313032a6d4cba9bf74f1ec3f84daa
Jorge Alexandro Zamudio Arredondo <alexzamudio_11@hotmail.com>
2023-12-11 08:08:12 -0700

    Add spaceships and spaceship details params for route navigation

--- lib/router/router.tsx
+++ lib/router/router.tsx
@@ -1,12 +1,13 @@
 import { CompositeScreenProps,NavigatorScreenParams } from "@react-navigation/native";
 import type { MaterialBottomTabScreenProps } from '@react-navigation/material-bottom-tabs';
 import { StackScreenProps } from "@react-navigation/stack";
-import { PeopleResult } from "../models/models";
+import { PeopleResult, SpaceshipsResult } from "../models/models";
 
 // Here we define the params for each tab screen
 export type RootTabParamList = {
   Home: undefined;
   People: NavigatorScreenParams<PeopleStackParamList>;
+  Spaceships: NavigatorScreenParams<SpaceshipStackParamList>;
 };
 
 // Here we define the params for each screen in the People tab
@@ -15,16 +16,19 @@ export type PeopleStackParamList = {
   PeopleDetailsPage: { person: PeopleResult; uri: string };
 };
 
+// Here we define the params for each screen in the Spaceships tab
+export type SpaceshipStackParamList = {
+  SpaceshipPage: undefined;
+  SpaceshipDetailsPage: { spaceship: SpaceshipsResult; uri: string };
+};
+
 // Here we define the props for each screen in the People tab
 export type PeopleStackScreenProps<T extends keyof PeopleStackParamList> =
 StackScreenProps<PeopleStackParamList, T>;
 
-// Here we define the props for each tab screen
-export type RootTabScreenProps<T extends keyof RootTabParamList> = 
-CompositeScreenProps<
-  MaterialBottomTabScreenProps<RootTabParamList, T>,
-  StackScreenProps<PeopleStackParamList>
->;
+// Here we define the props for each screen in the Spaceships tab
+export type SpaceshipStackScreenProps<T extends keyof SpaceshipStackParamList> =
+StackScreenProps<SpaceshipStackParamList, T>;
 
 // Here we declare the global namespace for react navigation
 declare global {
