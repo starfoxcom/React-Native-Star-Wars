530719a7a484caaa17b1bf8c2d26abcc9beb746b
Jorge Alexandro Zamudio Arredondo <alexzamudio_11@hotmail.com>
2023-12-11 22:56:50 -0700

    Add pagination functionality to all pages

--- lib/pages/people/view/people_page.tsx
+++ lib/pages/people/view/people_page.tsx
@@ -1,4 +1,4 @@
-import { Text, Image, View, TouchableOpacity, ImageBackground } from "react-native";
+import { Text, Image, View, TouchableOpacity, ImageBackground, Button } from "react-native";
 import { PeopleStackScreenProps } from "../../../router/router";
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
@@ -29,26 +29,44 @@ function PeoplePage({ navigation }: PeopleStackScreenProps<'PeoplePage'>) {
   </View>;
 
   return (
-    <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap' }}>
-      <GridView data={people} renderItem={(item) => (
-        <TouchableOpacity
-          style={{ justifyContent: 'center', alignItems: 'center' }}
-          onPress={() => navigation.navigate('PeopleDetailsPage', {
-            person: item, uri: `https://starwars-visualguide.com/assets/img/characters/${getUrlId(item.url)}.jpg`
-          })}
-        >
-          <ImageBackground source={
-            { uri: `https://starwars-visualguide.com/assets/img/big-placeholder.jpg` }}
-            imageStyle={{
-              width: 114, height: 110, borderTopLeftRadius: 10, borderTopRightRadius: 10,
-            }} >
-            <Image source={{
-              uri: `https://starwars-visualguide.com/assets/img/characters/${getUrlId(item.url)}.jpg`
-            }} style={{ width: 114, height: 110, borderTopLeftRadius: 10, borderTopRightRadius: 10 }} />
-          </ImageBackground>
-          <Text>{item.name}</Text>
-        </TouchableOpacity>
-      )} />
+    <View style={{justifyContent:"space-between", flex:1}}>
+      <View>
+        <GridView data={people} renderItem={(item) => (
+          <TouchableOpacity
+            style={{ justifyContent: 'center', alignItems: 'center' }}
+            onPress={() => navigation.navigate('PeopleDetailsPage', {
+              person: item, uri: `https://starwars-visualguide.com/assets/img/characters/${getUrlId(item.url)}.jpg`
+            })}
+          >
+            <ImageBackground source={
+              { uri: `https://starwars-visualguide.com/assets/img/big-placeholder.jpg` }}
+              imageStyle={{
+                width: 114, height: 110, borderTopLeftRadius: 10, borderTopRightRadius: 10,
+              }} >
+              <Image source={{
+                uri: `https://starwars-visualguide.com/assets/img/characters/${getUrlId(item.url)}.jpg`
+              }} style={{ width: 114, height: 110, borderTopLeftRadius: 10, borderTopRightRadius: 10 }} />
+            </ImageBackground>
+            <Text>{item.name}</Text>
+          </TouchableOpacity>
+        )} />
+      </View>
+      <View style={{ justifyContent: "space-evenly", flexDirection: 'row', bottom:10}}>
+        <Button
+          title="Previous"
+          disabled={!data?.previous}
+          onPress={() => {
+            setPage(page - 1)
+          }}
+        />
+        <Button
+          title="Next"
+          disabled={!data?.next}
+          onPress={() => {
+            setPage(page + 1)
+          }}
+        />
+      </View>
     </View>
   )
 }
